9dd483da75f0cf09d4dec62c9156eab1be94f638
Le Trung Nguyen <nguyenc2300110@student.ctu.edu.vn>
2025-03-08 11:03:28 +0700

    Update xong lan 2

--- CONFIG.js
+++ CONFIG.js
@@ -1,8 +1,8 @@
 const CONFIGDATA = {
   titleHeader: "Chúc người anh yêu 8/3 vui vẻ, luôn hạnh phúc và luôn yêu anh em nhé.À và  </br> Bé tha lỗi cho anh nhé :<",
-  buttonYes: "Tớ đồng ý",
+  buttonYes: "Em đồng ý",
   buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+  titleModar: "Cảm ơn em.",
   descriptionModar:
     "Cảm ơn bé iu đã iu anh. Dù anh còn nhiều thiếu sót và đã làm em buồn. Nhưng anh hứa, sẽ luôn yêu em, luôn bên em. Lần nữa chúc bé 8/3 thật tuyệt vời nhé. Yêu em",
 };
